4355762991b158e3fdc3bd5ba8a8fcb1a736fcd9
Cenk Keskin <cnkkskn001@gmail.com>
2026-04-14 20:43:26 +0300

    Important comment is added

--- Radar_Record.py
+++ Radar_Record.py
@@ -15,13 +15,14 @@ TEST_DEVICE         = 1             # 0 STM32F4, 1 STM32H7, 2 FPGA
 OPERATING_SYSTEM    = 1             # 0 MAC, 1 UBUNTU, 2 WINDOWS (Havent implemented serial on windows.)
 
 # important note: After changing R and C values of pll circuit freq ramp bw values are perfect.
-SWEEP_START         = 5.20e9 
-SWEEP_BW            = 1000e6
+# Notes: Current setup gives clean 50 MHz as well no noise at all on tune voltage ramp at 50 MHz (higher ramp is better any case)!!!
+SWEEP_START         = 5.60e9 
+SWEEP_BW            = 100e6
 TX_POWER_DBM        = 0
 SWEEP_TYPE          = 0             # 0 for Sawtooth, 1 for Triangular
 USE_PLL             = 1             # 0 for DAC, 1 for PLL
 TX_MODE             = 1             # 0 for continuous tx, 1 for on off with tx, 2 for testing when tx off
-GAIN                = 50            # 1 to 70 stmf4, 3 to 85 for H7
+GAIN                = 10            # 1 to 70 stmf4, 3 to 85 for H7
 DATA_LOG            = 1             # 0 for USB transfer, 1 for MicroCard Log
 SWEEP_TIME          = 250e-6        # 100 micro or 10 ms all working, sdcard log is designed for 128 chirp 250 micro for now
 CPI_CHIRP           = 128           # 1 for USB, 32 for 1ms SWEEP_TIME, 64 for 500, 128 for 250 (max)
