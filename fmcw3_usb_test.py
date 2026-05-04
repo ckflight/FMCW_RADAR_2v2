e59c81bebfd1c4ecf47f37300ca45958de6e51f4
Cenk Keskin <cnkkskn001@gmail.com>
2026-05-04 20:46:35 +0300

    multiwrite works.

--- fmcw3_usb_test.py
+++ fmcw3_usb_test.py
@@ -6,7 +6,7 @@ DEVICE_ID = "FTBJ7TCT"
 SYNCFF = 0x40
 SIO_RTS_CTS_HS = (0x1 << 8)
 
-TEST_PACKET1 = "1234512345123451234512345" # this one works
+TEST_PACKET1 = "123456789" # this one works
 
 dev = ftdi.Device(
     device_id=DEVICE_ID,
@@ -23,6 +23,10 @@ dev.flush()
 
 print("Packet1:", TEST_PACKET1)
 
+# multi read works as well. Ila sample size is important!!!
+# sendindg without delay is fine.
+dev.write(TEST_PACKET1)
+dev.write(TEST_PACKET1)
 dev.write(TEST_PACKET1)
 
 time.sleep(1.0)
