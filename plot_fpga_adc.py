0893e340fc8e9a256addd8facdf252db143df3c0
Cenk Keskin <cnkkskn001@gmail.com>
2026-05-16 10:23:38 +0300

    plot every nth chirp for faster plot

--- plot_fpga_adc.py
+++ plot_fpga_adc.py
@@ -9,9 +9,13 @@ import time
 FILENAME = "record.bin"
 
 SAMPLES_PER_CHIRP = 500
+
 START_CHIRP = 0
-END_CHIRP = None          # None = all chirps
-FRAME_DELAY = 0.005        # seconds between chirps
+END_CHIRP   = None
+
+CHIRP_STEP  = 10        # plot every Nth chirp
+
+FRAME_DELAY = 0.0001
 
 # ============================================================
 # READ RAW USB DATA
@@ -25,22 +29,36 @@ raw = raw[:valid_len]
 
 samples_u16 = np.frombuffer(raw, dtype=">u2")
 
-# FPGA sends: A[15:0], B[15:0]
+# FPGA sends:
+# A[15:0], B[15:0]
+
 adc_a_u12 = samples_u16[0::2] & 0x0FFF
 adc_b_u12 = samples_u16[1::2] & 0x0FFF
 
-# 12-bit centered ADC
+# convert 12-bit offset binary to centered signed
 adc_a = adc_a_u12.astype(np.int32) - 2048
 adc_b = adc_b_u12.astype(np.int32) - 2048
 
+# ============================================================
+# RESHAPE INTO CHIRPS
+# ============================================================
+
 num_chirps = len(adc_a) // SAMPLES_PER_CHIRP
+
 usable_samples = num_chirps * SAMPLES_PER_CHIRP
 
-adc_a_chirps = adc_a[:usable_samples].reshape(num_chirps, SAMPLES_PER_CHIRP)
-adc_b_chirps = adc_b[:usable_samples].reshape(num_chirps, SAMPLES_PER_CHIRP)
+adc_a_chirps = adc_a[:usable_samples].reshape(
+    num_chirps,
+    SAMPLES_PER_CHIRP
+)
 
-print("TOTAL BYTES:", len(raw))
-print("NUM CHIRPS:", num_chirps)
+adc_b_chirps = adc_b[:usable_samples].reshape(
+    num_chirps,
+    SAMPLES_PER_CHIRP
+)
+
+print("TOTAL BYTES :", len(raw))
+print("NUM CHIRPS  :", num_chirps)
 
 if END_CHIRP is None:
     END_CHIRP = num_chirps
@@ -63,17 +81,29 @@ ax.set_ylabel("ADC value, centered 12-bit")
 ax.grid(True)
 ax.legend()
 
-# fixed y-axis for stable view
+# fixed axis for stable animation
 y_min = min(adc_a.min(), adc_b.min())
 y_max = max(adc_a.max(), adc_b.max())
+
 ax.set_ylim(y_min - 100, y_max + 100)
 
-for chirp_idx in range(START_CHIRP, END_CHIRP):
+# ============================================================
+# PLAY CHIRPS
+# ============================================================
+
+for chirp_idx in range(
+    START_CHIRP,
+    END_CHIRP,
+    CHIRP_STEP
+):
 
     line_a.set_ydata(adc_a_chirps[chirp_idx])
     line_b.set_ydata(adc_b_chirps[chirp_idx])
 
-    ax.set_title(f"ADC Waveform - Chirp {chirp_idx + 1}/{num_chirps}")
+    ax.set_title(
+        f"ADC Waveform - Chirp "
+        f"{chirp_idx + 1}/{num_chirps}"
+    )
 
     fig.canvas.draw()
     fig.canvas.flush_events()
